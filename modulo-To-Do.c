75b7ca6c198683b96ddffc753f4476ade0730478
Ponce Matias Augusto <ponce.amtrk@gmail.com>
2026-04-30 20:10:48 -0300

    Comentarios

--- modulo-To-Do.c
+++ modulo-To-Do.c
@@ -1,21 +1,35 @@
+//____________________________________
+//BIBLIOTECAS
+//____________________________________
 #include<stdio.h>
 #include<stdlib.h>
 
+//____________________________________
+//ESTRUCTURAS
+//____________________________________
 struct
 {
     int TareaID; //Numérico autoincremental comenzando en 1000
-    char *Descripcion;
+    char * Descripcion;
     int Duracion; //entre 10 - 100
 }typedef Tarea;
 
 struct
 {
     Tarea T;
-    Nodo *Siguiente;
+    Nodo * Siguiente;
 }typedef Nodo;
 
 Nodo *CrearListaVacia();
 
+//____________________________________
+//DECLARACION DE FUNCIONES
+//____________________________________
+
+
+//____________________________________
+//FUNCION MAIN
+//____________________________________
 int main()
 {
     
@@ -23,6 +37,9 @@ int main()
     return 0;
 }
 
+//____________________________________
+//DEFINICION DE FUNCIONES
+//____________________________________
 Nodo *CrearListaVacia()
 {
     return NULL;
